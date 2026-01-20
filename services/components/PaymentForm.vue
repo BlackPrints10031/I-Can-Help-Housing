375058f1523d47c67131d9f9b094c2acbcdbd59f
BlackPrints10031 <blackprints10031@gmail.com>
2026-01-20 09:10:40 -0500

    Add PaymentForm component for Stripe payments

---
+++ services/components/PaymentForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="submitPayment">
+    <input type="number" v-model="amount" placeholder="Amount" />
+    <!-- Only show Stripe for personal accounts -->
+    <button class="btn btn-primary">Pay with Stripe</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      amount: 0,
+    };
+  },
+  methods: {
+    async submitPayment() {
+      await this.$axios.post('/api/payment/pay', {
+        amount: this.amount,
+        paymentMethod: 'stripe',
+      });
+      alert('Payment submitted!');
+    },
+  },
+};
+</script>
